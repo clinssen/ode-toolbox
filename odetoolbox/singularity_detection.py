60b99693589dda9964d85f2eb0fd2c2de7a5944c
C.A.P. Linssen <charl@turingbirds.com>
2026-03-19 10:42:15 +0100

    update docstring and fix private method naming

--- odetoolbox/singularity_detection.py
+++ odetoolbox/singularity_detection.py
@@ -127,19 +127,19 @@ class SingularityDetection:
         return filt_cond
 
     @staticmethod
-    def _generate_singularity_conditions(P: sympy.Matrix) -> List[Dict[sympy.core.expr.Expr, sympy.core.expr.Expr]]:
+    def _generate_singularity_conditions(P: sympy.Matrix) -> Set[Dict[sympy.core.expr.Expr, sympy.core.expr.Expr]]:
         r"""
-        The function solve returns a list where each element is a dictionary. And each dictionary entry (condition: expression) corresponds to a condition at which that expression goes to zero. If the expression is quadratic, like let's say "x**2-1" then the function 'solve() returns two dictionaries in a list. Each dictionary corresponds to one solution. We are then collecting these lists in our own list called ``conditions`` and return it.
+        For each element of ``P``, find conditions under which subterms of ``expr`` of the form ``a / b`` equal infinity (in general, when b = 0).
         """
         conditions = set()
         for expr in sympy.flatten(P):
-            conds = SingularityDetection.find_singularity_conditions_in_expression_(expr)
+            conds = SingularityDetection._find_singularity_conditions_in_expression(expr)
             conditions = conditions.union(conds)
 
         return conditions
 
     @staticmethod
-    def find_singularity_conditions_in_expression_(expr: sympy.core.expr.Expr) -> Set[SymmetricEq]:
+    def _find_singularity_conditions_in_expression(expr: sympy.core.expr.Expr) -> Set[SymmetricEq]:
         r"""Find conditions under which subterms of ``expr`` of the form ``a / b`` equal infinity (in general, when b = 0)."""
         conditions = set()
 
@@ -179,7 +179,7 @@ class SingularityDetection:
             particular_solution = -b[row] / A[row, row]
             particular_solution = sympy.simplify(particular_solution)    # using _custom_update_expr() does not guarantee that an adequate number of cases will be covered
 
-            conditions = conditions.union(SingularityDetection.find_singularity_conditions_in_expression_(particular_solution))
+            conditions = conditions.union(SingularityDetection._find_singularity_conditions_in_expression(particular_solution))
 
         conditions = SingularityDetection._filter_valid_conditions(conditions, A)  # filters out the invalid conditions (invalid means those for which A is not defined)
 
